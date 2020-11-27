cdeb920c3726baf03f45ce3f9bc301d974c1dab0
YQCao <caoyuqi1996@hotmail.com>
2020-11-27 10:05:56 +0100

    Using queue to implement priority scheduling.

---
+++ scheduler.c
@@ -0,0 +1,175 @@
+#include "scheduler.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+typedef struct _QueueItem {
+    /*
+     * The data of this item.
+     */
+    int data;
+    /*
+     * The next item in the queue.
+     * NULL if there is no next item.
+     */
+    struct _QueueItem *next;
+} QueueItem;
+
+typedef struct _Queue {
+    /*
+     * The first item of the queue.
+     * NULL if the queue is empty.
+     */
+    QueueItem *head;
+    /*
+     * The last item of the queue.
+     * undefined if the queue is empty.
+     */
+    QueueItem *tail;
+} Queue;
+
+typedef enum _ProcessState {
+    STATE_UNUSED = 0, // This entry in the _processes array is unused.
+
+    STATE_READY,      // The process is ready and should be on a ready queue for
+                      //   selection by the scheduler
+    STATE_RUNNING,    // The process is running and should not be on a ready queue
+    STATE_WAITING     // The process is blocked and should not be on a ready queue
+} ProcessState;
+
+typedef struct _Process {
+    int processId;
+    ProcessState state;
+    /*
+     * Range: 0 ... HIGHEST_PRIORITY (including)
+     * HIGHEST_PRIORITY is highest priority
+     */
+    int priority;
+} Process;
+
+Process _processes[MAX_PROCESSES] = {{0}};
+Queue priority_queues[HIGHEST_PRIORITY] = malloc(sizeof(Queue) * HIGHEST_PRIORITY);
+
+/* TODO: Add global variables if needed. */
+
+/*
+ * Adds a new, waiting process.
+ */
+int startProcess(int processId, int priority)
+{
+    if (((processId < 0) || (processId >= MAX_PROCESSES) ||
+        (_processes[processId].state != STATE_UNUSED)) ||
+        (priority < 0) || (priority > HIGHEST_PRIORITY)) {
+
+        return -1;
+    }
+
+    _processes[processId].processId = processId;
+    _processes[processId].state    = STATE_WAITING;
+    _processes[processId].priority = priority;
+    return 0;
+}
+
+/*
+ * Append to the tail of the queue.
+ * Does nothing on error.
+ */
+void _enqueue(Queue *queue, int data)
+{
+    QueueItem *p = malloc(sizeof(QueueItem));
+    assert(p != NULL);
+
+    p->data = data;
+    p->next = NULL;
+    if (queue->tail != NULL){
+        queue->tail->next = p;
+        queue->tail = p;
+    } else {
+        queue->head = p;
+        queue->tail = queue->head;
+    }
+}
+
+/*
+ * Remove and get the head of the queue.
+ * Return -1 if the queue is empty.
+ */
+int _dequeue(Queue *queue)
+{
+    if (queue->head == NULL) {
+        return -1;
+    }
+    
+    QueueItem *p = queue->head;
+    int data = p->data;
+
+    queue->head = p->next;
+    if (queue->tail == p) {
+        queue->tail = queue->head;
+    }
+
+    free(p);
+    return data;
+}
+
+void initScheduler()
+{
+}
+
+/*
+ * Called whenever a waiting process gets ready to run.
+ */
+void onProcessReady(int processId)
+{
+    assert(_processes[processId].state == ProcessState.STATE_READY);
+
+    switch(_processes[processId].priority) {
+        case 1:
+            _enqueue(queue[0], processId);
+            break;
+        case 2:
+            _enqueue(queue[1], processId);
+            break;
+        case 3:
+            _enqueue(queue[2], processId);
+            break;
+        case 4:
+            _enqueue(queue[3], processId);
+            break;
+        case 5:
+            _enqueue(queue[4], processId);
+            break;
+    }
+}
+
+/*
+ * Called whenever a running process is forced of the CPU
+ * (e.g., through a timer interrupt).
+ */
+void onProcessPreempted(int processId)
+{
+    (void)processId;
+
+    // TODO: Implement
+}
+
+/*
+ * Called whenever a running process is blocked (e.g., after requesting an I/O
+ * operation) and needs to wait.
+ */
+void onProcessBlocked(int processId)
+{
+    (void)processId;
+
+    // TODO: Implement
+}
+
+/*
+ * Gets the id of the next process to run and sets its state to running.
+ */
+int scheduleNextProcess()
+{
+    // TODO: Implement
+    return -1;
+}
